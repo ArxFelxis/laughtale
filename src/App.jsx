b8dc33d9e3f82867285c6ca238ebac892906d527
ArxFelxis <176923291+ArxFelxis@users.noreply.github.com>
2026-01-20 18:05:27 +0800

    Created card component & able to fetch data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState, useEffect } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App () {
+  const [team, setTeam] = useState([])
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const pokemonNames = ['bulbasaur', 'vulpix', 'abra', 'gastly', 'poliwag', 'sandshrew', 'machop', 'dratini', 'oddish', 'meowth', 'cubone', 'shellder']
+    const fetchPokemon = async () => {
+      try {
+        setLoading(true)
+        const results = await Promise.all(
+          pokemonNames.map(async poke => {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${poke}/`)
+            if(!response.ok) {
+              throw new Error('error: ' + response.status)
+            }
+            return await response.json()
+          })
+        )
+       setTeam(results)
+      }
+      catch (error){
+        setError(error.message)
+      }
+      finally {
+        setLoading(false)
+      }
+    }
+
+    fetchPokemon()
+  }, [])
+
+  if (loading) return <p>...Loading</p>
+
+  if (error) return <p>Error: {error}</p>
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Pokemon Memory Game</h1>
+    <p>Get points by clicking on an image but don't click on any more than once!</p>
+    <div className="cards-container">
+      {team.map(pokemon => 
+        <Card key={pokemon.id} pokemon={pokemon} />
+      )}
+    </div>
     </>
   )
 }
 
-export default App
+function Card ({pokemon}) {
+  return (
+    <button className='card' style={cardStyle}>
+      <img src={pokemon.sprites.front_default} alt={pokemon.name} style={img}/>
+      <p style={cardPara}>{pokemon.name.toUpperCase()}</p>
+    </button>
+  )
+}
+
+const cardStyle = {
+    width: '280px',
+    height: '350px',
+    overflow: 'hidden',
+    backgroundColor: 'white',
+    borderRadius: '15px',
+    fontSize: '23px',
+    fontFamily: "Galada",
+    margin: '10px',
+}
+
+const img = {
+  height: '200px'
+}
+
+const cardPara = {
+  color: 'black'
+}
